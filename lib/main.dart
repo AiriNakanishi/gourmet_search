1bf15655b86c7d88a3530cc710f1d167c4b93e98
AiriNakanishi <b1023165@fun.ac.jp>
2026-02-22 12:18:18 +0900

    アプリ名変更

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'kawaii Gourmet Search',
+      title: 'もぐあし',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.pinkAccent,
